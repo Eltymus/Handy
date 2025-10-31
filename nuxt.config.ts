0b2b74d0a2684c3306d4be1b2c8c603ad0f0a3d9
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-10-31 15:37:58 +0100

    stack 3d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@tresjs/nuxt", "@nuxt/devtools"],
+  tres: {
+    devtools: true,
+    glsl: true,
+  },
+});
